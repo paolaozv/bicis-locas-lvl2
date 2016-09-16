7f6c60a7291682f6df7356946b58ed31ab51a3c3
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-09-16 13:09:58 -0500

    modificacion

--- js/main.js
+++ js/main.js
@@ -1,52 +1,90 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+    var nombre = document.getElementById("name").value;
 	var apellido = document.getElementById("lastname").value;
 	var email = document.getElementById("input-email").value;
 	var contrasena = document.getElementById("input-password").value;
-	var nombre = document.getElementById("name").value;
-	var lista = document.getElementsByClassName("form-control").value;
-  
+    var lista = document.querySelector("select").value;
 
-  
-    var contenedor = document.getElementById("input-box");
-    var box = getElementById("texto");
-    contenedor.removeChild(box);
-    box.parentNode.removeChild(box);
-
-    
-    if(nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
-    	document.getElementById("texto").innerHTML = "Debe ingresar su nombre"; 
-   	}
-   	else {
+    if(nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)){
+        var span = document.createElement("span");
+        var referencia= document.getElementById("name");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Debe ingresar su nombre");
+        span.appendChild(texto);
+        return span;
+    }   else {
        var m = /^[a-zA-Z]*$/;
        if(!nombre.search(m)) {
             console.log("letra")
             m = /[a-z]/g;
         }    
         if(!nombre.search(m)){
-                alert("La primera letra debe empezar con maýuscula")
+            var span = document.createElement("span");
+            var referencia= document.getElementById("name");
+            var padre=referencia.parentNode;
+            padre.insertBefore(span,referencia);
+            var texto = document.createTextNode("La primera letra debe empezar con maýuscula");
+            span.appendChild(texto);
+            return span;
         }
-   	} 
+    }
 
-   	if(apellido == null || apellido.length == 0 || /^\s+$/.test(apellido)) {
-    	alert("Campo obligatorio");
-   	}
-   	else {
+    if(apellido == null || apellido.length == 0 || /^\s+$/.test(apellido)) {
+        var span = document.createElement("span");
+        var referencia= document.getElementById("lastname");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Debe ingresar su apellido");
+        span.appendChild(texto);
+        return span;
+    } else {
        var n = /^[a-zA-Z]*$/;
        if(!apellido.search(n)) {
             console.log("letra")
             n = /[a-z]/g;
         }    
         if(!apellido.search(n)){
-                alert("La primera letra debe empezar con maýuscula")
+            var span = document.createElement("span");
+            var referencia= document.getElementById("lastname");
+            var padre=referencia.parentNode;
+            padre.insertBefore(span,referencia);
+            var texto = document.createTextNode("La primera letra debe empezar con maýuscula");
+            span.appendChild(texto);
+            return span;
         }
-   	} 
-
-	if(!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email))) {
-		return alert("Ingresa tu email");
-	} else if( contrasena == null || contrasena.length <= 6 || contrasena == "password" || contrasena == 123456 || contrasena == 098754 || /^\s+$/.test(contrasena) ) {
-		return alert("Ingresa una contraseña válida");
-	} else if( lista.selectedIndex == null || lista.selectedIndex == 0 ) { 
-        return alert("Elegir una opción de  la lista");
     }
+
+    if(!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email))) {
+        var span = document.createElement("span");
+        var referencia= document.getElementById("input-email");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Verifique su email");
+        span.appendChild(texto);
+        return span;
+    } else if( contrasena == null || contrasena.length <= 6 || contrasena == "password" || contrasena == 123456 || contrasena == 098754 || /^\s+$/.test(contrasena) ) {
+        var span = document.createElement("span");
+        var referencia= document.getElementById("input-password");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Ingresa una contraseña válida");
+        span.appendChild(texto);
+        return span;
+    } 
+
+    if (lista == 0) {
+        var span = document.createElement("span");
+        var referencia= document.querySelector("select");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Debes seleccionar al menos un tipo de bici");
+        span.appendChild(texto);
+        return span;
+        }
 }
+
+
+
+
